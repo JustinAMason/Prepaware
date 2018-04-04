5b8df85d6109453adbb01b4b15286a4d5e341247
Justin Mason <justinmason@172-18-46-35.DYNAPOOL.NYU.EDU>
2018-04-04 14:55:28 -0400

    create mongoose schemas

--- src/db.js
+++ src/db.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
 
-// add your schemas
-// use plugins (for slug)
-// register your model
+// Add your schemas
+const RecipeSchema = new mongoose.Schema({
+    name: String,
+    servings: Number,
+    ingredients: [],
+    price: Number,
+    cals: Number,
+    carbs: Number,
+    fat: Number,
+    protein: Number
+});
 
+const ItemSchema = new mongoose.Schema({
+    category: String,
+    brand: String,
+    name: String,
+    weight: Number,
+    price: Number,
+    cals: Number,
+    carbs: Number,
+    fat: Number,
+    protein: Number
+});
+
+// Use plugins (for slug)
+RecipeSchema.plugin(URLSlugs("title"));
+ItemSchema.plugin(URLSlugs("title"));
+
+// Register your model
+mongoose.model("Recipe", RecipeSchema);
+mongoose.model("Item", ItemSchema);
+
+// Connect MongoDB (locally, temporarily)
 mongoose.connect('mongodb://localhost/prepaware');
