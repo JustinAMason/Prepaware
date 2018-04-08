4222c1f6e4bf1415ab56023b26c637aeeae30049
Justin Mason <justinmason@172-18-46-134.DYNAPOOL.NYU.EDU>
2018-04-08 00:34:30 -0400

    implement mongoose auto-incrementation

--- src/db.js
+++ src/db.js
@@ -1,8 +1,33 @@
 // Bring-in Modules
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
+const autoIncrement = require("mongoose-auto-increment");
+
+// Configure database
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, 'config.json');
+    const data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // conenction string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/final_project';
+}
+
+// Initialize auto-incrementing
+const connection = mongoose.createConnection(dbconf);
+autoIncrement.initialize(connection);
 
 // Add your schemas
+
     // Schema for created recipes
     const RecipeSchema = new mongoose.Schema({
         name: String,
@@ -32,26 +57,24 @@ const URLSlugs = require('mongoose-url-slugs');
 RecipeSchema.plugin(URLSlugs("name"));
 ItemSchema.plugin(URLSlugs("name"));
 
+// Use plugins (for auto-increment)
+    RecipeSchema.plugin(autoIncrement.plugin, {
+        model: "name",
+        field: "recipeID",
+        startAt: 1000,
+        incrementBy: 1
+    });
+
+    ItemSchema.plugin(autoIncrement.plugin, {
+        model: "name",
+        field: "recipeID",
+        startAt: 1000,
+        incrementBy: 1
+    });
+
 // Models registration
 mongoose.model("Recipe", RecipeSchema);
 mongoose.model("Item", ItemSchema);
 
-let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    // if we're in PRODUCTION mode, then read the configration from a file
-    // use blocking file io to do this...
-    const fs = require('fs');
-    const path = require('path');
-    const fn = path.join(__dirname, 'config.json');
-    const data = fs.readFileSync(fn);
-
-    // our configuration file will be in json, so parse it and set the
-    // conenction string appropriately!
-    const conf = JSON.parse(data);
-    dbconf = conf.dbconf;
-} else {
-    // if we're not in PRODUCTION mode, then use
-    dbconf = 'mongodb://localhost/final_project';
-}
-
+// Connect to configured database
 mongoose.connect(dbconf);
