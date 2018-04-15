8b550006da0771b81d4d3a128d6bfb3240a84749
Justin Mason <justinmason@172-18-41-58.DYNAPOOL.NYU.EDU>
2018-04-15 18:06:34 -0400

    implement gulp automation

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const sass = require('gulp-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./src/public/sass/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/public/css'));
 });
 
-gulp.task('sass:watch', function() {
+gulp.task("watch", function() {
     gulp.watch('./src/public/sass/*.scss', ['sass']);
 });
+
+gulp.task("default", ["watch"]);
