4c73a896ac799cdcbae5952e67c685d8276a4d1e
Justin Mason <justinmason@172-17-87-99.DYNAPOOL.NYU.EDU>
2018-04-19 14:02:37 -0400

    add gulp-eslint code to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const eslint = require("gulp-eslint");
 
 gulp.task('sass', function () {
     return gulp.src('./src/public/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/public/css'));
+});
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task("watch", function() {
